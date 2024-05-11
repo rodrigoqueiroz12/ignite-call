ac378e9edc3381c376d42d91ca493ccd6f8bff30
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-05-11 10:48:14 -0300

    update calendar

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -2,7 +2,7 @@ interface GetWeekDaysParams {
   short?: boolean
 }
 
-export function getWeekDays({ short }: GetWeekDaysParams) {
+export function getWeekDays({ short }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
