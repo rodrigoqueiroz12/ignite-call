ecc64e01ea9c0a9e3ec5ba3c3e6e611221995a83
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-05-14 09:11:23 -0300

    update calendar

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,4 +1,6 @@
 import { CaretLeft, CaretRight } from '@phosphor-icons/react'
+import dayjs from 'dayjs'
+import { useState } from 'react'
 
 import { getWeekDays } from '@/utils/get-week-days'
 
@@ -12,20 +14,38 @@ import {
 } from './styles'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonth = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonth)
+  }
+
+  function handleNextMonth() {
+    const nextMonth = currentDate.add(1, 'month')
+
+    setCurrentDate(nextMonth)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
--- src/pages/schedule/[username]/schedule-form/index.tsx
+++ src/pages/schedule/[username]/schedule-form/index.tsx
@@ -1,6 +1,5 @@
 import { CalendarStep } from './calendar-step'
-import { ConfirmStep } from './confirm-step/index.page'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
+  return <CalendarStep />
 }
